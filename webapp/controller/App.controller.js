5d6c101839fcee1692d11a4d49c0fd4ebe6bacfe
kalyani viswanadhuni <kalyanif0004@gmail.com>
2024-11-07 06:25:08 +0000

    first commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("com.app.capacitymanagement.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comapp/capacitymanagement/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
